9478970c0af387df8abd6366a02a41ea953d25ee
Balwant <49509457+ballu1190@users.noreply.github.com>
2022-04-16 18:24:37 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 
 
-
+app = Flask(__name__)
 @app.route('/')
 def home():
 	return render_template('index.html')
-app = Flask(__name__)
+
 if __name__ == "__main__":
     app.run()
